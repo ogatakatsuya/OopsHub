b759c35df5f392008e8e4655168f3f8579318c65
NaokiIkesaka <ik.11.naoki@gmail.com>
2024-05-12 10:31:15 +0900

    adminにmodelを登録

--- backend/SNS/admin.py
+++ backend/SNS/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post, Room, Message
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Room)
+admin.site.register(Message)
