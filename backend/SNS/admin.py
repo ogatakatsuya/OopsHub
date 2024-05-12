f60420373e3d1741f23a4b27dfbe9d7d51a2d907
NaokiIkesaka <ik.11.naoki@gmail.com>
2024-05-12 10:37:29 +0900

    adminにuser登録

--- backend/SNS/admin.py
+++ backend/SNS/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Room, Message
+from .models import Post, Room, Message, User
 
 admin.site.register(Post)
 admin.site.register(Room)
 admin.site.register(Message)
+admin.site.register(User)
