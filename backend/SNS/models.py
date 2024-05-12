8480653f42761aa0b4e9d9db22f662fd44aa4554
syarasyoujyu <143589461+syarasyoujyu@users.noreply.github.com>
2024-05-13 06:42:52 +0900

    modelの修正

--- backend/SNS/models.py
+++ backend/SNS/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name=models.CharField(max_length=100)
-    password=models.TextField()
+    password=models.CharField(max_length=100)
     created_at=models.DateTimeField()
     updated_at=models.DateTimeField(null=True)
     def __str__(self):
