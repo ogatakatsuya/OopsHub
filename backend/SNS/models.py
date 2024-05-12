4d2337f08e6e41a408e573cb1b5fdb2561ae3dc9
syarasyoujyu <143589461+syarasyoujyu@users.noreply.github.com>
2024-05-13 06:00:01 +0900

    modelの修正（ver1）

--- backend/SNS/models.py
+++ backend/SNS/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name=models.CharField()
+    name=models.CharField(max_lenght=100)
     password=models.TextField()
     created_at=models.DateTimeField()
     updated_at=models.DateTimeField(null=True)
@@ -10,7 +10,7 @@ class User(models.Model):
         return self.name
 
 class Post(models.Model):
-    text=models.CharField()
+    text=models.CharField(max_length=100)
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="user_posts")
     created_at=models.DateTimeField()
     def __str__(self):
@@ -26,7 +26,7 @@ class Room(models.Model):
 class Message(models.Model):
     room_id=models.ForeignKey(Room,on_delete=models.CASCADE,related_name="room_messages")
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="user_messages")
-    message=models.CharField()
+    message=models.CharField(max_length=100)
     def __str__(self):
         return self.message
     
