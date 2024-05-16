7c240f253d612a8e1b90de86f3bec76a3a5f28e9
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-05-17 08:53:14 +0900

    for merge

--- backend/SNS/views.py
+++ backend/SNS/views.py
@@ -225,7 +225,7 @@ def contest(request):
     if request.method=="GET":
         serializer_get = ContestSerializer(contests, many=True)
         contests = serializer_get.data
-        return Response({"message": contests})
+        return Response({"message": "hello world"})
     
     if request.method=="POST":
         data=request.data.copy()
