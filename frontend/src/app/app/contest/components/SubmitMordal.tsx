6e6ef9e34a540d266cc327af87e9fe45dae72b4d
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-05-17 20:54:28 +0900

    for merge

---
+++ frontend/src/app/app/contest/components/SubmitMordal.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Input,
+    Button,
+    Textarea
+} from '@chakra-ui/react'
+
+import { useState } from 'react';
+import { auth } from '@/app/firebase';
+
+const SubmitMordal = ({ isOpen, onClose, onOpen, contest_id }) => {
+    
+    const [ text, setText ] = useState("");
+    const [ submitError, setSubmitError ] = useState("");
+    const handleSubmit = async () => {
+        const user_id = auth.currentUser?.uid; // ユーザーIDを正しく取得
+        // const now = new Date();
+        // const year = now.getFullYear();
+        // const month = String(now.getMonth() + 1).padStart(2, '0');
+        // const day = String(now.getDate()).padStart(2, '0');
+        // const hours = String(now.getHours()).padStart(2, '0');
+        // const minutes = String(now.getMinutes()).padStart(2, '0');
+        // const seconds = String(now.getSeconds()).padStart(2, '0');
+        // const created_at = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+        try {
+            const res = await fetch(`http://localhost:8000/contest/${contest_id}/post`, { // ポート番号を修正
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ user_id: user_id, text: text }),
+            });
+
+            if (!res.ok) {
+            const errorData = await res.json();
+            setSubmitError(errorData.message || "何か問題が発生しました");
+            } else {
+            const data = await res.json();
+            console.log(data);
+            }
+        } catch (err) {
+            setSubmitError("ネットワークエラーです。後で再試行してください。");
+            console.error("ネットワークエラー:", err);
+        }
+        };
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+            <ModalHeader>コンテストに投稿！</ModalHeader>
+            <ModalCloseButton />
+            <ModalFooter>
+            <Textarea
+            size="lg"
+            p={3}
+            bgColor="white"
+            variant="flushed"
+            value={text}
+            onChange={(e) => {setText(e.target.value)}}
+            />
+            <Button
+            colorScheme="blue"
+            size="md"
+            bgColor="white"
+            variant="outline"
+            px={7}
+            type="submit"
+            onClick={handleSubmit}
+            >
+            投稿
+            </Button>
+            </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default SubmitMordal;
