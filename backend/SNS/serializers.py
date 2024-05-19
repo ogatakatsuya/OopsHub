8cb6afe70b31c6d6b7bd69a5db7d64428a89caa9
NaokiIkesaka <ik.11.naoki@gmail.com>
2024-05-19 13:04:16 +0900

    serializer

--- backend/SNS/serializers.py
+++ backend/SNS/serializers.py
@@ -10,7 +10,7 @@ class AISolutionSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=["id","name","passowrd","created_at","updated_at"]
+        fields=["id","name","created_at","updated_at"]
 
 class PostSerializer(serializers.ModelSerializer):
     content=serializers.CharField()
