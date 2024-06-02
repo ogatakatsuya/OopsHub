538d3a8b1ec818c6e2fc4ab0c12f9dceee84518f
NaokiIkesaka <ik.11.naoki@gmail.com>
2024-06-02 22:30:23 +0900

    import修正

--- backend/SNS/urls.py
+++ backend/SNS/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import LikeCreateDestroyView, DontMindCreateDestroyView,LearnedCreateDestroyView,LLMView,VoteCreateDestroyView,ContestDeleteView,SignUpView
 
 
 urlpatterns = [
@@ -8,13 +7,13 @@ urlpatterns = [
     path('post/',views.PostView.as_view(),name="Post_get_post"),
     path("post/<int:pk>",views.App_modify,name="modify_app"),
     path("contest/",views.contest,name="contest"),
-    path("contest/<int:contest_id>",ContestDeleteView.as_view(),name="contest-delete"),
+    path("contest/<int:contest_id>",views.ContestDeleteView.as_view(),name="contest-delete"),
     path("contest/<int:contest_id>/post",views.contestroom,name="contestroom"),
-    path("name",SignUpView.as_view(),name="signup"),
+    path("name",views.SignUpView.as_view(),name="signup"),
     path("contest/<int:contest_id>/post/<int:post_id>",views.PostDeleteView.as_view(),name="delete-post"),
-    path("contest/<int:post_id>/vote",VoteCreateDestroyView.as_view(),name="vote-create"),
-    path('like/<int:post_id>/',  LikeCreateDestroyView.as_view(), name='like-create-destroy'),
-    path('dontmind/<int:post_id>/', DontMindCreateDestroyView.as_view(), name='dontmind-create-destroy'),
-    path('learned/<int:post_id>/', LearnedCreateDestroyView.as_view(), name='learned-create-destroy'),
-    path('api/',LLMView.as_view(),name='AI-solution'),
+    path("contest/<int:post_id>/vote",views.VoteCreateDestroyView.as_view(),name="vote-create"),
+    path('like/<int:post_id>/',  views.LikeCreateDestroyView.as_view(), name='like-create-destroy'),
+    path('dontmind/<int:post_id>/', views.DontMindCreateDestroyView.as_view(), name='dontmind-create-destroy'),
+    path('learned/<int:post_id>/', views.LearnedCreateDestroyView.as_view(), name='learned-create-destroy'),
+    path('api/',views.LLMView.as_view(),name='AI-solution'),
 ]
